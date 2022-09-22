b401a90cd1129ec7eba380e176907ff91cc0ce09
MrLento234 <lento.manickathan@gmail.com>
2022-09-22 17:22:00 +0200

    added brownian motion example

---
+++ docs/art/brownian_motion/main.js
@@ -0,0 +1,37 @@
+//Browning motion
+// Description: Particle system browning motion
+// Date: 22.09.2022
+
+// Global variables
+const width = 600;
+const height = 600;
+
+const xmid = width / 2;
+const ymid = height / 2;
+
+const n_particles = 100;
+
+let p = [];
+
+function setup() {
+  let canvas = createCanvas(width, height);
+  canvas.parent("canvas");
+  
+  for (let i = 0; i < n_particles; i++) {
+    let x = random(width);
+    let y = random(height);
+    let dx = random(-5, 5);
+    let dy = random(-5, 5);
+    let size = random(1, 5);
+    p.push(new Point(x, y, dx, dy, size));
+  }
+
+}
+
+function draw() {
+  clear();
+  for (let i=0; i < n_particles; i++) {
+    p[i].move();
+    p[i].draw();
+  }
+}
