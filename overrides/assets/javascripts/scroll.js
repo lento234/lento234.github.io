b230e5ff95d36314fcd050fbc046e8cf88a9c311
MrLento234 <lento.manickathan@gmail.com>
2022-10-03 16:32:06 +0200

    improved stylization

--- overrides/assets/javascripts/scroll.js
+++ overrides/assets/javascripts/scroll.js
@@ -3,20 +3,17 @@ var subHeading = document.getElementsByClassName("subheading")[0]
 
 const defaultSubheadingFontSize = subHeading.style.fontSize;
 
-
-    // trigger this function every time the user scrolls
+// trigger this function every time the user scrolls
 window.onscroll = function (event) {
     var scroll = window.pageYOffset;
     var windowWidth = window.innerWidth;
 
     if ((scroll > 1) && (windowWidth > 1000)) {
-        // green
         heading.style.color = "black";
-        heading.style.transition = "1s";
-
+        heading.style.transition = ".5s";
         subHeading.style.color = "#ff5252";
-        subHeading.style.transition = "1.5s";
-        subHeading.style.fontSize = "1.25rem";
+        subHeading.style.transition = "cubic-bezier(.165,.84,.44,1) 1.5s";
+        subHeading.style.fontSize = "1.5rem";
 
     } else if (windowWidth > 1000) {
         heading.style.color = "white";
